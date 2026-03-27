be77dc7974f0a49004a49713ab4d38f04178c67d
Ishita Das <ishitad123@gmail.com>
2026-03-27 15:48:43 +0530

    Adding Application Insights

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Azure.Cosmos;
 using Order.Infrastructure.Services;
 using Order.Application.Interfaces;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,9 @@ builder.Services.AddScoped<IMessagePublisher, ServiceBusPublisher>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Application Insight
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
